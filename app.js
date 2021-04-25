71ef7541d351e4698f7a0b85b2bc3c2c3cbb7244
Navya-Shree- MN <navyashreemn296@gmail.com>
2021-04-25 20:10:14 +0530

    last

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-// const express = require('express');
-// const hbs = require('hbs')
-// const app = express();
+const express = require('express');
+const hbs = require('hbs')
+const app = express();
 
-// app.use(express.static(__dirname+'/static'))
-// app.use(express.json());
-// app.use(express.urlencoded({extended:false}));
-
-
-// app.set('views',__dirname+'/views')
-// app.set('view engine','hbs')
-
-// app.get('/home',function(req,res) {
-//     res.render('homepage')
-// })
-
-// app.get('/catalogue',(req,res)=>{
-//     res.render('manageProducts')
-// })
+app.use(express.static(__dirname+'/static'))
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 
+app.set('views',__dirname+'/views')
+app.set('view engine','hbs')
 
-// app.listen(3000,() => {console.log("server is running in 3000")});
+app.get('/home',function(req,res) {
+    res.render('homepage')
+})
+
+app.get('/catalogue',(req,res)=>{
+    res.render('manageProducts')
+})
+
+
+
+app.listen(3000,() => {console.log("server is running in 3000")});
